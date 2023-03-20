a294e4cdbe0d05b36f8408ab7c877bdd5a9cb71d
Ashfaaq Rifath <ashfaaq.rifath2@gmail.com>
2023-03-20 12:59:02 +0530

    Update server.py

--- server.py
+++ server.py
@@ -1,39 +1,93 @@
-import socket
 import threading
+import socket
+import os
+import colorama
+from colorama import Fore, Back
+colorama.init(autoreset=True)
 
-HEADER = 64
-PORT = 5050
-SERVER = socket.gethostbyname(socket.gethostname())
-ADDR = (SERVER, PORT)
-FORMAT = "utf-8"
-DISCONNECT_MSG = "!DISCONNECT"
+host = "192.168.8.166"
+port = 5050
 
 server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-server.bind(ADDR)
+server.bind((host, port))
+server.listen()
 
-def handle_client(conn, addr):
-    print(f"NEW CONNECTION {addr} connected.")
+clients = []
+nicknames = []
 
-    connected = True
-    while connected:
-        msg_length = conn.recv(HEADER).decode(FORMAT)
-        if msg_length:
-            msg_length = int(msg_length)
-            msg = conn.recv(msg_length).decode(FORMAT)
-            if msg == DISCONNECT_MSG:
-                connected = False
+os.system("cls")
 
-            print(f"[{addr}] {msg}")
-    conn.close()
+# 1.Broadcasting Method
+def broadcast(message):
+    for client in clients:
+        client.send(message)
 
-def start():
-    server.listen()
-    print(f"[LISTENING] Server is listening on {SERVER}")
+# 2.Recieving Messages from client then broadcasting
+def handle_client(client):
     while True:
-        conn, addr = server.accept()
-        thread = threading.Thread(target=handle_client, args=(conn, addr))
-        thread.start()
-        print(f"ACTIVE CONNECTIONS {threading.activeCount() -1}")
+        try:
+            msg = message = client.recv(1024)
+            if msg.decode("ascii").startswith("KICK"):
+                if nicknames[clients.index(client)] == "admin":
+                    name_to_kick = msg.decode("ascii")[5:]
+                    kick_user(name_to_kick)
+                else:
+                    client.send("Command Refused!".encode("ascii"))
+            else:
+                broadcast(message)   # As soon as message recieved, broadcast it.
 
-print("Server is loading...")
-start()
+        except:
+            if client in clients:
+                index = clients.index(client)
+                #Index is used to remove client from list after getting diconnected
+                client.remove(client)
+                client.close
+                nickname = nicknames[index]
+                broadcast(f"{nickname} left the Chat!".encode("ascii"))
+                nicknames.remove(nickname)
+                break
+
+# Main Recieve method
+def recieve():
+    while True:
+        client, address = server.accept()
+        print(f"Connected with {str(address)}")
+        # Ask the clients for Nicknames
+        client.send("NICK".encode("ascii"))
+        nickname = client.recv(1024).decode("ascii")
+        # If the Client is an Admin promopt for the password.
+        
+
+        if nickname == "admin":
+            client.send("PASS".encode("ascii"))
+            password = client.recv(1024).decode("ascii")
+            # I know it is lame, but my focus is mainly for Chat system and not a Login System
+            if password != "adminpass":
+                client.send("REFUSE".encode("ascii"))
+                client.close()
+                continue
+
+        nicknames.append(nickname)
+        clients.append(client)
+
+        print(f"Nickname of the client is {nickname}")
+        broadcast(f"{nickname} joined the Chat".encode("ascii"))
+        client.send("Connected to the Server!".encode("ascii"))
+
+        # Handling Multiple Clients Simultaneously
+        thread = threading.Thread(target=handle_client, args=(client,))
+        thread.start()
+
+def kick_user(name):
+    if name in nicknames:
+        name_index = nicknames.index(name)
+        client_to_kick = clients[name_index]
+        clients.remove(client_to_kick)
+        client_to_kick.send("You Were Kicked from Chat".encode("ascii"))
+        client_to_kick.close()
+        nicknames.remove(name)
+        broadcast(f"{name} was kicked from the server".encode("ascii"))
+
+#Calling the main method
+print("Server is Listening ...")
+recieve()
